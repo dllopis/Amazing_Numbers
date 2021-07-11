28d2ba7312de5b02c5ff89affae5c95699a6af9e
dllopis <iistrideii@gmail.com>
2021-07-10 20:09:36 -0400

    Completed Spy Numbers - Part 5/8 -

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -1,86 +1,27 @@
 package numbers;
+import java.text.DecimalFormat;
 import java.util.*;
 
 public class Main {
     private final static Scanner scanner = new Scanner(System.in);
-    private boolean even;
-    private boolean odd;
-    private boolean buzz;
-    private boolean duck;
-    private boolean gapful;
-    private boolean palindrome;
+    private final DecimalFormat formatter = new DecimalFormat("#,###");
+    private Map<String, Boolean> map;
+    private final Set<String> properties;
 
-    private void displayProperty(String input) {
-        System.out.printf("Properties of %s%n" + "even: %b%n" + "odd: %b%n" + "buzz: %b%n" +
-                "duck: %b%n" + "palindromic: %b%n" + "gapful: %b%n%n", input,
-                even, odd, buzz, duck, palindrome, gapful);
+    public Main()   {
+        this.properties = new HashSet<>();
+        this.properties.add("even");
+        this.properties.add("odd");
+        this.properties.add("buzz");
+        this.properties.add("duck");
+        this.properties.add("gapful");
+        this.properties.add("palindromic");
+        this.properties.add("spy");
     }
-    private void displayProperties(String input)    {
-        System.out.println(input + " is " +
-                (even ? "even," : "odd, ") +
-                (buzz ? "buzz, " : "") +
-                (duck ? "duck, " : "") +
-                (gapful ? "gapful" : "") +
-                (palindrome ? "palindromic" : "")
-        );
-    }
-    public void run()   {
-        displayInstructions();
-        String input;
-        do {
-            System.out.print("Enter a request: ");
-            input = scanner.nextLine();
-        } while (parseInput(input));
-    }
-    private void displayInstructions()  {
-        System.out.println("Welcome to Amazing Numbers!\n\n" +
-                "Supported requests:\n" +
-                "- enter a natural number to know its properties;\n" +
-                "- enter two natural numbers to obtain the properties of\n" +
-                "the list:\n" +
-                "* the first parameter represents a startng number;\n" +
-                "* the second parameter shows how many consecutive\n" +
-                "numbers are to be printed;\n" +
-                "- separate the parameters with one space;\n" +
-                "- enter 0 to exit.\n");
-    }
-    private boolean parseInput(String input)   {
-        if(input.matches(""))   {
-            displayInstructions();
-        }else if(input.matches("[-][0-9]+") || input.matches("[^0-9]+")
-                || input.matches("[-][0-9]+\\s[0-9]+")|| input.matches("[^0-9]+\\s[0-9]+")){
-            System.out.println("The first parameter should be a natural number or zero.");
-        }else if(input.matches("[0-9]+\\s[^0-9]+") || input.matches("[0-9]+\\s-[0-9]+")
-        || input.matches("[0-9]+\\s[^0-9]*")){
-            System.out.println("The second parameter should be a natural number or zero.");
-        }else if (input.equals("0")) {
-            System.out.println("\n\nGoodbye!");
-            return false;
-        }else if(input.matches("\\d+"))   {
-            setProperties(input);
-            displayProperty(input);
-        }else if(input.matches("\\d+\\s+\\d+"))  {
-            String[] request = input.split(" ");
-            int len = Integer.parseInt(request[1]);
-
-            for(int i = 0; i < len; i++) {
-                long num = Long.parseLong(request[0]) + i;
-                setProperties(num + "");
-                displayProperties(num + "");
-            }
-        }
-        return true;
-    }
-    private boolean isEven(long input)  {
-        return input % 2 == 0;
-    }
-    private boolean isOdd(long input)  {
-        return input % 2 == 1;
-    }
-    private boolean isBuzzNumber(Long input)    {
-        return input % 7 == 0 || input % 10 == 7;
-    }
-    private boolean isDuckNumber(String input) {
+    private boolean isEven(long input)  {   return input % 2 == 0;  }
+    private boolean isOdd(long input)   {   return input % 2 == 1;  }
+    private boolean isBuzz(long input)  {   return input % 7 == 0 || input % 10 == 7;   }
+    private boolean isDuck(String input)    {
         return input.charAt(0) == '0' && input.substring(1).contains("0")
                 || input.charAt(0) != '0' && input.contains("0");
     }
@@ -99,18 +40,185 @@ public class Main {
         return input.length() >= 3 && Long.parseLong(input) % Long.parseLong(input.charAt(0) + ""
                 + input.charAt(input.length() - 1)) == 0;
     }
+    private boolean isSpy(String input) {
+        String[] digits = input.split("");
+        long sum = 0;
+        long product = 1;
+
+        for (String digit : digits) {
+            long num = Long.parseLong(digit);
+            sum += num;
+            product *= num;
+        }
+        return sum == product;
+    }
+    /*
+        Populate our map with boolean values of each property, so that
+        we can have them readily accessible.
+     */
     private void setProperties(String input)    {
         long num = Long.parseLong(input);
+        this.map.put("even", isEven(num));
+        this.map.put("odd", isOdd(num));
+        this.map.put("buzz", isBuzz(num));
+        this.map.put("duck", isDuck(input));
+        this.map.put("gapful", isGapful(input));
+        this.map.put("palindromic", isPalindromic(input));
+        this.map.put("spy", isSpy(input));
+    }
+    /*
+        Prints out to console whether each property is true of false
+     */
+    private void displayProperty(String input)  {
+        System.out.printf("Properties of %s%n" +
+                        "even: %b%n" + "odd: %b%n" +
+                        "buzz: %b%n" +
+                        "duck: %b%n" +
+                        "palindromic: %b%n" +
+                        "gapful: %b%n" +
+                        "spy: %b%n%n" ,
+                this.formatter.format(Long.parseLong(input)),
+                this.map.get("even"),
+                this.map.get("odd"),
+                this.map.get("buzz"),
+                this.map.get("duck"),
+                this.map.get("palindromic"),
+                this.map.get("gapful"),
+                this.map.get("spy")
+        );
+    }
+    /*
+        Prints out to console the input value with properties equal to true.
+     */
+    private void displayProperties(String input)    {
+        Iterator<Map.Entry<String, Boolean>> itr = this.map.entrySet().iterator();
+        StringBuilder sb = new StringBuilder();
 
-        this.even = isEven(num);
-        this.odd = isOdd(num);
-        this.buzz = isBuzzNumber(num);
-        this.duck = isDuckNumber(input);
-        this.gapful = isGapful(input);
-        this.palindrome = isPalindromic(input);
+        if(itr.hasNext())   {
+            sb.append(this.formatter.format(Long.parseLong(input))).append(" is ");
+        }
+        while(itr.hasNext()) {
+            Map.Entry<String, Boolean> e1 = itr.next();
+            sb.append(e1.getValue() ? itr.hasNext() ? e1.getKey() + ", " : e1.getKey() : "");
+        }
+        if(sb.charAt(sb.length() - 2) == ',')   {
+            sb.setLength(sb.length() - 2);
+        }
+        System.out.println(sb);
+    }
+    /*
+        Handles error output.
+     */
+    private void displayCodeMsg(int code, String userInput) {
+        switch(code)    {
+            case 0  :   {
+                System.out.println("\n\nGoodbye!");
+                break;
+            }
+            case 1  :   {
+                System.out.println("The first parameter should be a natural number or zero.");
+                break;
+            }
+            case 2  :   {
+                System.out.println("The second parameter should be a natural number.");
+                break;
+            }
+            case 3  :   {
+                System.out.printf("The property [%s] is wrong.\n" +
+                        "Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY]%n%n", userInput);
+                break;
+            }
+            default :   break;
+        }
+    }
+    /*
+        Criteria to pass challenge
+     */
+    private void displayInstructions()  {
+        System.out.println("Welcome to Amazing Numbers!\n\n" +
+                "Supported requests:\n" +
+                "- enter a natural number to know its properties;\n" +
+                "- enter two natural numbers to obtain the properties of\n" +
+                "the list:\n" +
+                "* the first parameter represents a starting number;\n" +
+                "* the second parameter shows how many consecutive\n" +
+                "numbers are to be printed;\n" +
+                "two natural numbers and a property to search for;" +
+                "- separate the parameters with one space;\n" +
+                "- enter 0 to exit.\n");
+    }
+    /*
+        This method is in need of some refactoring and doesn't cover all the basis other then
+        what is needed to complete the challenge.
+        Simply, it checks if the input matches a certain structure with regex.
+     */
+    private boolean parseInput(String input)   {
+        this.map = new HashMap<>();
+        if(input.equals(""))   {
+            displayInstructions();
+        }else if(input.matches("[-][0-9]+") || input.matches("[^0-9]+")
+                || input.matches("[-][0-9]+\\s[0-9]+")|| input.matches("[^0-9]+\\s[0-9]+")){
+            displayCodeMsg(1, "");
+        }else if(input.matches("[0-9]+\\s[^0-9]+") || input.matches("[0-9]+\\s-[0-9]+")
+        || input.matches("[0-9]+\\s[^0-9]*")){
+            displayCodeMsg(2, "");
+        }else if (input.equals("0")) {
+            displayCodeMsg(0, "");
+            return false;
+        }else if(input.matches("\\d+\\s\\d+\\s[^a-zA-Z]+") || input.matches("\\d+\\s\\d+\\s\\d+"))  {
+            String[] temp = input.split(" ");
+            displayCodeMsg(3, temp[2]);
+        }else if(input.matches("\\d+"))   {
+            map = new HashMap<>();
+            setProperties(input);
+            displayProperty(input);
+        }else if(input.matches("\\d+\\s\\d+"))  {
+            String[] request = input.split(" ");
+            int len = Integer.parseInt(request[1]);
+
+            for(int i = 0; i < len; i++) {
+                map = new HashMap<>();
+                long num = Long.parseLong(request[0]) + i;
+                setProperties(num + "");
+                displayProperties(num + "");
+            }
+        }else if(input.matches("\\d+\\s\\d+\\s[a-zA-Z]+"))   {
+            String[] data = input.split(" ");
+            if(properties.contains(data[2]))   {
+                long start = 0;
+                int end = Integer.parseInt(data[1]);
+                int count = 0;
+
+                while(start < Long.MAX_VALUE && count < end)  {
+                    this.map = new HashMap<>();
+                    long num = Long.parseLong(data[0]) + (start++);
+                    setProperties(num + "");
+
+                    if(map.get(data[2]))    {
+                        displayProperties(num + "");
+                        count++;
+                    }
+                }
+            }   else    {
+                displayCodeMsg(3, data[2].toUpperCase());
+            }
+        }
+        return true;
+    }
+    /*
+        Driver method to be called in main.
+        Helps avoid clutter.
+     */
+    public void run()   {
+        displayInstructions();
+        String input;
+        do {
+            System.out.print("Enter a request: ");
+            input = scanner.nextLine();
+        } while (parseInput(input.toLowerCase()));
+        scanner.close();
     }
     public static void main(String[] args) {
-        Main buzzNumber = new Main();
-        buzzNumber.run();
+        new Main().run();
     }
 }
